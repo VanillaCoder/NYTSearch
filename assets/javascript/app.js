069dbc9274f0be9559208370da33beac0e3575db
Jim Tatarakis <jimtatarakis@gmail.com>
2019-04-13 12:29:45 -0700

    psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+
+
+//  TODO: NYT api get.
+//  TODO: write API call.
+//  TODO: call will need to have a dynamic url.
+//  TODO: when calling use info that user inputs.
+//  TODO: after call write.then fxn.
+//  TODO: display articles in html.
+//  TODO: clear articles fxn.
+//  $("button").on("click", function() {
+    // var person = $(this).attr("data-person");
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    //   person + "&api_key=dc6zaTOxFJmzC&limit=10";
