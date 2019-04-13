03cf11f0030ce4f97de6293d4994070e4375378a
Alex Engelmann <alex.engelmann@gmail.com>
2019-04-13 13:23:00 -0700

    not an email

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,5 @@
-//  TODO: NYT api get.
-//  TODO: write API call.
-//  TODO: call will need to have a dynamic url.
-//  TODO: when calling use info that user inputs.
-//  TODO: after call write.then fxn.
+
+
 //  TODO: display articles in html.
 //  TODO: clear articles fxn.
 //  $("button").on("click", function() {
@@ -10,6 +7,31 @@
     // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     //   person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-var apiKey = b97PVFoem0M22HUVjcdSHpLd6ky0e9Wz;
-var term = "";
+var apiKey = "b97PVFoem0M22HUVjcdSHpLd6ky0e9Wz";
+var term = $("#searchTerm").text;
+var recNumber = $("#numberOfRec").text;
+
+//TODO add to url
+var startYear = $("#startYear").text;
+var endYear = $("#endYear").text;
+
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + term + "&api-key=" + apiKey;
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response) {
+    
+
+    console.log(response);
+    console.log(response.response.docs[0].headline.main);
+    console.log(response.response.docs[0].snippet);
+
+
+
+}
+)
+
+
