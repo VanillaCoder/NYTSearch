5d94afe4b153b1a468e71492ddc0eda4118107db
Alex Engelmann <alex.engelmann@gmail.com>
2019-04-13 12:31:01 -0700

    query setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//TODO 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var apiKey = b97PVFoem0M22HUVjcdSHpLd6ky0e9Wz;
+var term = "";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + term + "&api-key=" + apiKey;
